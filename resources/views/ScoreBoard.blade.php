a288555ca97827e50f85656d581049ef0fd1b3db
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-02-13 01:34:30 +0600

    Bootstarp Scaffolding and layout setup
    add bootstrap scaffolding and setup layout and also add all the js and css to the layout

---
+++ resources/views/ScoreBoard.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4 p-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h2>2000</h2>
+                        <h5>Live Score</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+
+@endsection
---
+++ resources/views/ScoreUpdate.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4 p-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5>Update Score</h5>
+                        <hr>
+                        <input type="text" class="form-control ScoreValue"><br>
+                        <button class="btn btn-block btn-success">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+
+@endsection
